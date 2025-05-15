26f474092bf66972fd1663ab4f29857eef550168
Ohi20 <ashrafohi20@gmail.com>
2025-05-15 16:40:21 +0600

    banner created

---
+++ mern-client/src/home/FavoriteBooks.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const FavoriteBooks = () => {
+  const [books, setbooks] = useState([]);
+  return <div>FavoriteBooks</div>;
+};
+
+export default FavoriteBooks;
