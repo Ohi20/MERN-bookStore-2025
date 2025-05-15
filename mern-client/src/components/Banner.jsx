d6cbdcb756eda7cb7361c9e2684841e7132e4b42
Ohi20 <ashrafohi20@gmail.com>
2025-05-15 11:54:41 +0600

    banner slider added

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import BannerCard from "../home/BannerCard";
 
 const Banner = () => {
   return (
     <div className="px-4 lg:px-24 bg-teal-100 flex items-center">
       <div className="w-full flex flex-col md:flex-row justify-between items-center gap-12 py-40">
         {/* left side */}
-        <div>Left Side</div>
+        <div className="md:w-1/2 space-y-8 h-full">
+          <h2 className="text-6xl font-bold leading-snug text-black">
+            Buy & Sell Your Books{" "}
+            <span className="text-blue-700">For The Best Prices</span>
+          </h2>
+          <p className="md:w-4/5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            voluptates culpa dignissimos officia voluptatum sit tempore illo,
+            reiciendis debitis inventore. Magnam beatae recusandae architecto
+            minus perferendis itaque quo excepturi ipsum?
+          </p>
+          <div>
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="search a book"
+              className="py-2 px-2 rounded-s-sm outline-none"
+            />
+            <button className="bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200">
+              search
+            </button>
+          </div>
+        </div>
         {/* right side */}
-        <div>Right Side</div>
+        <div>
+          <BannerCard />
+        </div>
       </div>
     </div>
   );
