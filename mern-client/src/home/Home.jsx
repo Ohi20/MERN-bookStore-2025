32cb46859b9a9b615ccdafe9cf905f4ea0ba0a4c
Ohi20 <ashrafohi20@gmail.com>
2025-05-20 09:52:50 +0600

    homepage section update

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import FavoriteBooks from "./FavoriteBooks";
+import ListedBooks from "./ListedBooks";
 
 const Home = () => {
   return (
     <div className="h-screen">
       <Banner />
       <FavoriteBooks />
+      <ListedBooks />
     </div>
   );
 };
